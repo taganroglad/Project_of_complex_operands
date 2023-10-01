cb8ae1256c3ccbf641e0b76fbbfa2d56fb7f2582
Alexey agafonov <taganroglad1@gmail.com>
2023-10-01 10:42:23 +0300

    just crated a foler

---
+++ man_cat_robot/man_cat_robot/src/Human.java
@@ -0,0 +1,29 @@
+class Human {
+    private String name;
+    private int maxRunDistance;
+    private int maxJumpHeight;
+
+    public Human(String name, int maxRunDistance, int maxJumpHeight) {
+        this.name = name;
+        this.maxRunDistance = maxRunDistance;
+        this.maxJumpHeight = maxJumpHeight;
+    }
+
+    public void run(int distance) {
+        if (distance <= maxRunDistance) {
+            System.out.println(name + " run successfully " + distance + " meters");
+        } else {
+            System.out.println(name + " couldn't run succesfully " + distance + " meters");
+        }
+    }
+
+    public void jump(int height) {
+        if (height <= maxJumpHeight) {
+            System.out.println(name + " Jump over the wall, hights of " + height + " meters");
+        } else {
+            System.out.println(name + " couldn't jump over the wall, hights " + height + " meters");
+        }
+    }
+}
+
+
