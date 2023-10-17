09559431049dcd712d9fb947bc8a21c1299daeb1
taganroglad <110729432+taganroglad@users.noreply.github.com>
2023-10-17 13:54:15 +0300

    Delete calc/calc directory

--- calc/calc/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        CalculatorModel model = new CalculatorModel();
-        CalculatorView view = new CalculatorView();
-        CalculatorPresenter presenter = new CalculatorPresenter(model, view);
-        view.setVisible(true);
-    }
-}
